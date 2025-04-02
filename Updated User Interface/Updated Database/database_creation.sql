2b9d56ea8dd20e7dc3e46abbf1529fab6fba4da2
Chukwubuikem Nwafor <kamsonwafor@protonmail.ch>
2025-04-01 22:02:43 -0500

    Updated Database
    New Database for User Interface

---
+++ Updated User Interface/Updated Database/database_creation.sql
@@ -0,0 +1,86 @@
+CREATE TABLE IF NOT EXISTS warehouses (
+    warehouse_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    warehouse_desc TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS battery_state (
+    state_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    state_desc TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS works (
+    work_type_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    work_type_name TEXT NOT NULL,
+	parent_work_type_id int,
+	foreign KEY (parent_work_type_id) REFERENCES works (work_type_id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS employees (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    password TEXT NOT NULL,
+    is_admin INTEGER NOT NULL,
+    warehouse_id INTEGER,
+    FOREIGN KEY (warehouse_id) REFERENCES warehouses(warehouse_id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS locations (
+    location_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    location_shorthand TEXT,
+    location_description TEXT NOT NULL,
+    warehouse_id INTEGER,
+    FOREIGN KEY (warehouse_id) REFERENCES warehouses(warehouse_id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS batteries (
+    battery_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    serial_number TEXT UNIQUE,
+    part_number INTEGER,
+    item_type INTEGER,
+    part_description TEXT NOT NULL,
+    quantity_total INTEGER,
+    quantity_new Integer,
+    quantity_old Integer,
+    quantity_death_row Integer,
+    location_new integer,
+    location_old integer,
+    location_death_row integer,
+    picture BLOB,
+    FOREIGN KEY (location_new) REFERENCES locations(location_id) ON DELETE SET NULL,
+    FOREIGN KEY (location_old) REFERENCES locations(location_id) ON DELETE SET NULL,
+    FOREIGN KEY (location_death_row) REFERENCES locations(location_id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS requests (
+    request_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    request_time TIMESTAMP UNIQUE,
+    serial_number TEXT,
+    work_type_id INTEGER,
+    user_id INTEGER,
+    warehouse_id INTEGER,
+    state_id INTEGER,
+    quantity INTEGER,
+    FOREIGN KEY (serial_number) REFERENCES batteries(serial_number) ON DELETE SET NULL,
+    FOREIGN KEY (work_type_id) REFERENCES works(work_type_id) ON DELETE SET NULL,
+    FOREIGN KEY (user_id) REFERENCES employees(user_id) ON DELETE SET NULL,
+    FOREIGN KEY (warehouse_id) REFERENCES warehouses(warehouse_id) ON DELETE SET NULL,
+    FOREIGN KEY (state_id) REFERENCES battery_state(state_id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS reports (
+    request_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    report_desc TEXT NOT NULL,
+    FOREIGN KEY (request_id) REFERENCES requests(request_id) ON DELETE SET NULL
+);
+
+/*
+drop table if exists batteries;
+drop table if exists battery_state;
+drop table if exists employees;
+drop table if exists locations;
+drop table if exists reports;
+drop table if exists requests;
+drop table if exists warehouses;
+drop table if exists works;
+*/
---
+++ Updated User Interface/Updated Database/database_mock_values.py
@@ -0,0 +1,126 @@
+import sqlite3
+
+# Establish connection to SQLite3 database
+conn = sqlite3.connect('spiers_database.db')
+
+# Enable foreign key support
+conn.execute('PRAGMA foreign_keys = ON;')
+
+# Link cursor
+cursor = conn.cursor()
+
+# Insert each warehouse into the warehouse table
+warehouses = [
+    ("Warehouse 1",),
+    ("Warehouse 2",),
+    ("Warehouse 3",)
+]
+
+for warehouse_desc in warehouses:
+    cursor.execute('''
+        INSERT INTO warehouses (warehouse_desc)
+        VALUES (?)
+    ''', (warehouse_desc[0],))
+
+# Insert each employee into the employees table
+employees = [
+    ("John", "Smith", "123password", 1, 1),
+    ("Emily", "Johnson", "p123assword", 0, 1),
+    ("Michael", "Williams", "pa123ssword", 0, 1),
+    ("Sarah", "Brown", "pas123sword", 1, 2),
+    ("James", "Davis", "pass123word", 0, 2),
+    ("Jessica", "Wilson", "passw123ord", 0, 2),
+    ("David", "Moore", "passwo123rd", 1, 3),
+    ("Olivia", "Taylor", "passwor123d", 0, 3),
+    ("Daniel", "Anderson", "password123", 0, 3)
+]
+
+for first_name, last_name, password, is_admin, warehouse_id in employees:
+    cursor.execute('''
+        INSERT INTO employees (first_name, last_name, password, is_admin, warehouse_id)
+        VALUES (?, ?, ?, ?, ?)
+    ''', (first_name, last_name, password, is_admin, warehouse_id))
+
+# Insert each location into the location table
+locations = [
+    ("FS1, W1", "floor space 1, warehouse 1", 1),
+    ("FS2, W1", "floor space 2, warehouse 1", 1),
+    ("FS3, W1", "floor space 3, warehouse 1", 1),
+    ("SS1, W1", "shelf space 1, warehouse 1", 1),
+    ("SS2, W1", "shelf space 2, warehouse 1", 1),
+    ("SS3, W1", "shelf space 3, warehouse 1", 1),
+    ("FS1, W2", "floor space 1, warehouse 2", 2),
+    ("FS2, W2", "floor space 2, warehouse 2", 2),
+    ("FS3, W2", "floor space 3, warehouse 2", 2),
+    ("SS1, W1", "shelf space 1, warehouse 1", 2),
+    ("SS2, W1", "shelf space 2, warehouse 1", 2),
+    ("SS3, W1", "shelf space 3, warehouse 1", 2)
+]
+
+for location_shorthand, location_description, warehouse_id in locations:
+    cursor.execute("""
+        INSERT INTO locations (location_shorthand, location_description, warehouse_id)
+        VALUES (?, ?, ?)
+    """, (location_shorthand, location_description, warehouse_id))
+
+# Insert each battery state into the location table
+battery_state = [
+    ("New",),
+    ("Old",),
+    ("Death Row",)
+]
+
+# Insert each employee into the employees table
+for state_desc in battery_state:
+    cursor.execute("""
+        INSERT INTO battery_state (state_desc)
+        VALUES (?)
+    """, (state_desc[0],))
+
+batteries = [
+    ("battery_1_serial", 1, 1, "battery 1", 20, 10, 5, 5, 1, 2, 3),
+    ("battery_2_serial", 1, 1, "battery 2", 20, 10, 5, 5, 4, 5, 6),
+    ("battery_3_serial", 1, 1, "battery 3", 20, 10, 5, 5, 7, 8, 9)
+]
+
+for serial_number, part_number, item_type, part_description, quantity_total, quantity_new, quantity_old, quantity_death_row, location_new, location_old, location_death_row in batteries:
+    cursor.execute("""
+        INSERT INTO batteries (serial_number, part_number, item_type, part_description, quantity_total, quantity_new, quantity_old, quantity_death_row, location_new, location_old, location_death_row)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (serial_number, part_number, item_type, part_description, quantity_total, quantity_new, quantity_old, quantity_death_row, location_new, location_old, location_death_row))
+
+values = [
+    (1, "Find", None),
+    (2, "Receive", 1),
+    (3, "Ship", 1),
+    (4, "Move", 1),
+    (5, "Update Battery Status", 1),
+    (6, "New Battery Work", 5),
+    (7, "Old Battery Work", 5),
+    (8, "Death Row Battery Work", 5),
+    (9, "Store", 6),
+    (10, "Diagnostic Analysis", 7),
+    (11, "Disassembly", 7),
+    (12, "Repair", 7),
+    (13, "Re-assembly", 7),
+    (14, "Testing", 7),
+    (15, "Re-certification", 7),
+    (16, "Take Apart", 8),
+    (17, "Shred (pieces)", 8),
+    (18, "Shred (powder)", 8),
+    (19, "Make new battery", 8),
+    (20, "Take Picture", None),
+    (21, "Intake New Item", None)
+]
+
+for work_type_id, work_type_name, parent_work_type_id in values:
+    cursor.execute("""
+        INSERT INTO WORKS (work_type_id, work_type_name, parent_work_type_id)
+        VALUES (?, ?, ?)
+    """, (work_type_id, work_type_name, parent_work_type_id))
+
+# Commit changes and close connection
+conn.commit()
+
+# Close the connection
+conn.close()
