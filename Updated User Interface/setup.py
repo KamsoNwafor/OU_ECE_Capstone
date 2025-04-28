16afb24899cc5d8d6197746f59a4b4aba86a93bb
Samkelo14 <98428315+Samkelo14@users.noreply.github.com>
2025-04-28 17:32:13 -0500

    Add files via upload

--- Updated User Interface/setup.py
+++ Updated User Interface/setup.py
@@ -5,5 +5,5 @@ setup(
     name="MyApp",
     version="1.0",
     description="My Raspberry Pi GUI App",
-    executables=[Executable("NewUI.py", base="GUI")]
+    executables=[Executable("NewUI.py")]
 )
